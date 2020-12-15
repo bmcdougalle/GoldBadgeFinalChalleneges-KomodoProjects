a0492906a8df5d0c7672ca0b7b2c4c3ad1cf9293
bmcdougalle <bmcdougalle@gmail.com>
2020-12-15 17:36:37 -0500

    Added The Create Method in CompanyOuting repo

--- 04_CompanyOutings_Repo/companyOutings_Repo.cs
+++ 04_CompanyOutings_Repo/companyOutings_Repo.cs
@@ -8,5 +8,11 @@ namespace _04_CompanyOutings_Repo
 {
     public class companyOutings_Repo
     {
+        private readonly List<CompanyOutings> _companyOutings = new List<CompanyOutings>();
+
+        public void AddOuting(CompanyOutings outing)
+        {
+            _companyOutings.Add(outing);
+        }
     }
 }
